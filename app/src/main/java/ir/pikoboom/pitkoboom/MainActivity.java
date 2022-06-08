0496b189e23647ed95255fd2f406a9b27b0d4f59
miladjamshidpur <miladjamshidpur@gmail.com>
2022-06-09 03:48:57 +0430

    Initial commit

---
+++ app/src/main/java/ir/pikoboom/pitkoboom/MainActivity.java
@@ -0,0 +1,14 @@
+package ir.pikoboom.pitkoboom;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
